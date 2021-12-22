e415f4aaddef023068e09ed4a2a2ebb6344c434a
Mikael Elí <mikaeleli221@gmail.com>
2021-12-22 14:35:33 +0000

    Revert "feat: add utility types"
    This reverts commit fd954c04a3458e9ef6fae073abc531f5a9ed0435.

--- src/types.ts
+++ src/types.ts
@@ -21,25 +21,3 @@ export interface BemProps extends BemPropsModifier {
 	/** CSS BEM class-name prefix to be used for this component. Defaults to the same as the original component's displayName */
 	bem?: string;
 }
-
-export type InferTypeFromArray<A> = A extends Array<infer Data> ? Data : never;
-
-export type RecursivePartial<T> = {
-	[P in keyof T]?: RecursivePartial<T[P]>;
-};
-
-export type Nullable<T> = T | null;
-
-export type Maybe<T> = Nullable<T> | undefined;
-
-export type SparseArray<V> = Array<V | undefined>;
-
-export type SaneRecord<K extends string | number | symbol, V> = string extends K
-	? Record<K, V | undefined>
-	: number extends K
-	? Record<K, V | undefined>
-	: symbol extends K
-	? Record<K, V | undefined>
-	: Record<K, V>;
-
-export type ExcludesFalse = <T>(x: T | null | undefined | false) => x is T;
